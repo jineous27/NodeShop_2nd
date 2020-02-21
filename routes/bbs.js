19979188f9fbfc4d8a20c4de06759979d93e6770
jinheeyi <jinhee.yi27@gmail.com>
2020-02-22 08:54:00 +1000

    Add bbs.js & Install Nodemon

---
+++ routes/bbs.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var router = express.Router();
+
+//게시판에 글 등록하는 API
+router.post('/', (req, res) => {
+    res.json({
+        message:"it's posted"
+    });
+});
+
+//게시판에 글 받아오는 API
+router.get('/', (req, res) => {
+    res.json({
+        message:"it's retrieved"
+    });
+});
+
+//게시판에 글 업데이트 API
+router.patch('/', (req, res) => {
+    res.json({
+        message: "it's updated"
+    });
+});
+
+//게시판에 글 삭제하는 API
+router.delete('/', (req, res) => {
+    res.json({
+        message: "it's deleted"
+    });
+});
+
+
+module.exports = router;
--- server.js
+++ server.js
@@ -12,10 +12,13 @@ app.use((req, res) => {
 
 var productRoutes = require('./routes/products');
 var cartRoutes = require('./routes/carts');
+var bbsRoutes = require('./routes/bbs');
 
 
 app.use('/products', productRoutes);
 app.use('/carts', cartRoutes);
+app.use('/bbs', bbsRoutes);
+
 
 var PORT = 1010;
 
